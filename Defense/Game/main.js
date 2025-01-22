749a306d359eaeec6c4812e14aa953398cd0e622
pupungeggang2 <kyongkyong2892@gmail.com>
2025-01-22 14:53:32 +0900

    Start

--- Defense/Game/main.js
+++ Defense/Game/main.js
@@ -1,1 +1,14 @@
-console.log('Hello')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
